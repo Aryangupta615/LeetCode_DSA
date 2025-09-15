9470713346463e7eaa6e426441943de9016eba45
Aryangupta615 <aryan.gupta4_cs23@gla.ac.in>
2025-09-15 17:46:18 +0530

    Time: 0 ms (100%), Space: 43.2 MB (38.97%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -15,27 +15,7 @@ class Solution {
                 right = mid - 1; // Move the right pointer
             }
         }
-
         // Target not found; return the insertion position
         return left;
     }
-
-    // public static void main(String[] args) {
-    //     Solution solution = new Solution();
-
-    //     // Example 1
-    //     int[] nums1 = {1, 3, 5, 6};
-    //     int target1 = 5;
-    //     System.out.println("Output: " + solution.searchInsert(nums1, target1)); // Output: 2
-
-    //     // Example 2
-    //     int[] nums2 = {1, 3, 5, 6};
-    //     int target2 = 2;
-    //     System.out.println("Output: " + solution.searchInsert(nums2, target2)); // Output: 1
-
-    //     // Example 3
-    //     int[] nums3 = {1, 3, 5, 6};
-    //     int target3 = 7;
-    //     System.out.println("Output: " + solution.searchInsert(nums3, target3)); // Output: 4
-    // }
 }
