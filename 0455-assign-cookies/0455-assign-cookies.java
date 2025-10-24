96fa6dcd26377eb9208630c50efa155113fb92ae
Aryangupta615 <aryan.gupta4_cs23@gla.ac.in>
2025-10-24 13:17:23 +0530

    Time: 10 ms (80.29%), Space: 49.1 MB (9.97%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -5,7 +5,7 @@ class Solution {
         int i=0;
         int j=0;
         while(i<g.length && j<s.length){
-            if(g[i]<=s[j]){
+            if(g[i] <= s[j]){
                 i++;
             }
             j++;
